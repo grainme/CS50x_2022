3b94ff9d4e2a1d626671e086a080965a2bedb453
MarouaneBouf <104838272+MarouaneBouf@users.noreply.github.com>
2022-08-11 10:31:35 +0100

    Update array_size.c

--- Lectures/array_size.c
+++ Lectures/array_size.c
@@ -4,6 +4,14 @@
 
 // In this case sizeof(arr) == 40 bytes and sizeof(arr[0])==sizeof(int)==4 bytes --- > which gives by division 10 
 
+// A pointer variables holds an address, yes. So no matter what is stored at the bytes beginning at that address, the actual address is 8 bytes.
+
+/* char *name = "here's a really long string!";  
+   - > sizeof(name) will be 8
+   char name[] = "here's a really long string!";
+   - > sizeof(name) will be 29
+   -------> In both cases, strlen(name) will be 28 */
+
 
 #include <stdio.h>
 
